7299e33e1d1397a806d63489c9be508ed915a243
Dongjae Park <derpykraken0421@gmail.com>
2022-02-25 14:58:40 +0900

    should start playing but doesn't

--- music_cog.py
+++ music_cog.py
@@ -13,7 +13,7 @@ class music_cog(commands.Cog):
         self.vc = None
         self.current_channel = None
 
-        self.YDL_OPTIONS = {'format': 'bestaudio'}
+        self.YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
         self.FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 - reconnect_delay_max 5', 'options': '-vn'}
     
     def search_yt(self, item):
@@ -27,9 +27,22 @@ class music_cog(commands.Cog):
 
         return {'url': url, 'title': title}
 
-    async def start_play(self):
+    async def start_play(self, ctx):
         if len(self.queue) > 0:
-            self.is_playing = True
+            current_url = self.queue[0]['url']
+            current_title = self.queue[0]['title']
+            self.queue.pop(0)
+            self.vc.play(discord.FFmpegPCMAudio(current_url, **self.FFMPEG_OPTIONS), after = lambda e: self.next())
+            print('start playing')
+            await ctx.send(f'{current_title}')
+
+    def next(self):
+        if len(self.queue) > 0:
+            current_url = self.queue[0]['url']
+            current_title = self.queue[0]['title']
+            self.queue.pop(0)
+            self.vc.play(discord.FFmpegPCMAudio(current_url, **self.FFMPEG_OPTIONS), after = lambda e: self.next())
+
 
 
 
@@ -43,11 +56,12 @@ class music_cog(commands.Cog):
     async def join(self, ctx):
         voice_channel = ctx.author.voice.channel
         try:
-            self.vc = await ctx.guild.change_voice_state(channel = voice_channel)
-            #self.vc = await voice_channel.connect()
+            #   self.vc = await ctx.guild.change_voice_state(channel = voice_channel)
+            self.vc = await voice_channel.connect()
             self.current_channel = voice_channel
         except AttributeError:
             await ctx.send("Join a voice channel", delete_after = 5)
+        print('is_connected' , self.vc.is_connected())
         #except discord.errors.ClientException:
             #await ctx.send("Mewsic is already connected to a channel", delete_after = 5)
         #self.voice = discord.utils.get(self.bot.voice_clients, guild = ctx.guild)
@@ -65,14 +79,14 @@ class music_cog(commands.Cog):
                 await ctx.send("Join a voice channel", delete_after = 5)
                 return 0
             except discord.errors.ClientException:
-                await ctx.send("Mewsic is already connected to a channel", delete_after = 5)
+                await ctx.send("Mewsic is already connected to another channel", delete_after = 5)
                 return 0
         
-        # song = self.search_yt(searching_for)
-        # self.queue.append(song)
+        song = self.search_yt(searching_for)
+        self.queue.append(song)
 
-        # if self.is_playing:
-
+        if self.vc.is_playing() == False:
+            await self.start_play(ctx)
         
 
 
@@ -89,7 +103,8 @@ class music_cog(commands.Cog):
     async def on_voice_state_update(self, member, before, after):
         print('state update')
         if member.bot and before.channel != None and after.channel == None:
-            self.vc = None
-            self.current_channel = None
-            self.queue = []
-            print(self.vc)
+            self.vc.cleanup()
+            # self.current_channel = None
+            # self.queue = []
+            # print(self.vc)
+            #print('is_connected', self.vc.is_connected())
